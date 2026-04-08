656d2467e112dc9d1a7e90e8f239f315866fe605
Shahrukh Khan <srkplayer47@gmail.com>
2026-04-08 23:39:32 +0530

    fixing sever for itch.io

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
